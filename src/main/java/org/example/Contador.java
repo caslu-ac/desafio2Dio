bb4cf349a52d70d2ae2ae69e9df7d5de26d207e5
caslu-ac <lucas.confessor10@gmail.com>
2024-03-26 22:22:25 -0300

    projeto finalizado

--- src/main/java/org/example/Contador.java
+++ src/main/java/org/example/Contador.java
@@ -1,6 +1,37 @@
 package org.example;
 
 import java.util.Scanner;
-public class Main {
-    
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parametro deve ser maior que o primeiro");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+    if (parametroUm > parametroDois){
+        throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+
+    }
+    for (int i = parametroUm; i <= parametroDois; i++) {
+        System.out.println("Imprimindo numero" + i);
+    }
+    }
+}
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
 }
